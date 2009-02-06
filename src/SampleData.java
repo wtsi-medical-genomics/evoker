db7b90bb39a8fee24a157f2b1dd93753606a636a
jcbarret <>
2009-02-06 12:17:02 +0000

    throws its IOException

--- src/SampleData.java
+++ src/SampleData.java
@@ -9,28 +9,25 @@ public class SampleData {
 
     Vector<String> inds;
 
-    SampleData(String famFilename){
+    SampleData(String famFilename) throws IOException{
 
         this.inds = new Vector<String>();
 
-        try{
-            BufferedReader famReader = new BufferedReader(new FileReader(famFilename));
-            String currentLine;
-            StringTokenizer st;
-            while ((currentLine = famReader.readLine()) != null){
-                st = new StringTokenizer(currentLine);
-                while (st.hasMoreTokens()){
-                    st.nextToken();
-                    inds.add(st.nextToken());
-                    //todo: put remaining data somewhere
-                    break;
-                }
+        BufferedReader famReader = new BufferedReader(new FileReader(famFilename));
+        String currentLine;
+        StringTokenizer st;
+        while ((currentLine = famReader.readLine()) != null){
+            st = new StringTokenizer(currentLine);
+            while (st.hasMoreTokens()){
+                st.nextToken();
+                inds.add(st.nextToken());
+                //todo: put remaining data somewhere
+                break;
             }
-            famReader.close();
-
-        }catch (IOException ioe){
-            //todo: complain
         }
+        famReader.close();
+
+
     }
 
     public String getInd(int i){
