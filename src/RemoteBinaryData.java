1a0c3d657fd78833be60ae97826f059e1f3f91f4
james_morris81 <>
2009-11-10 14:51:53 +0000

    added a description/outline of a checkFile() method for checking the total size of remote uncompressed files to make sure they are the correct size before proceeding- needs to be coded

--- src/RemoteBinaryData.java
+++ src/RemoteBinaryData.java
@@ -1,3 +1,11 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import com.sshtools.j2ssh.sftp.SftpFile;
+
 
 public abstract class RemoteBinaryData extends BinaryData{
 
@@ -8,8 +16,24 @@ public abstract class RemoteBinaryData extends BinaryData{
         this.dc = dc;
     }
 
-    public void checkFile(byte[] header){
-        //TODO: how to be able to run this check without unnecessarily slowing down the system?
-    }
-    
+//    public void checkFile(String filename){
+//        //TODO: how to be able to run this check without unnecessarily slowing down the system?
+//    	// get the size of the bed or bnt file
+//    	// compare this to what we are expecting
+//    	
+//    	// first get the file name 
+//    	// use evoker-helper.pl to get the size of the file?
+//    	String[] out = new String[1];
+//    	
+//    	
+//    	out[i] = ((SftpFile)files.get(i)).;
+//    	
+//    	if (file.length() != (numSNPs*bytesPerRecord) + headers.length){
+//    		if (file.length() == (numSNPs*bytesPerRecord) + 8){
+//    				
+//    		} else{
+//    			throw new IOException(file + " is not properly formatted.\n(Incorrect length.)");
+//    		}
+//    	}    	
+//    }        
 }
