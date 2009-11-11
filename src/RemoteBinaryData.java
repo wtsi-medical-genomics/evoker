3341a28a1f5c97ad4afd225f069d1e37b5f2fbfd
james_morris81 <>
2009-11-11 16:36:49 +0000

    got the method checkFile() working for default format remote bed and bnt files

--- src/RemoteBinaryData.java
+++ src/RemoteBinaryData.java
@@ -1,11 +1,4 @@
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-
-import com.sshtools.j2ssh.sftp.SftpFile;
-
 
 public abstract class RemoteBinaryData extends BinaryData{
 
@@ -16,23 +9,16 @@ public abstract class RemoteBinaryData extends BinaryData{
         this.dc = dc;
     }
 
-    public void checkFile(String filename){
-//      //TODO: how to be able to run this check without unnecessarily slowing down the system?
-//    	// get the size of the bed or bnt file
-//    	// compare this to what we are expecting
-//    	
-//    	// first get the file name 
-//    	// use evoker-helper.pl to get the size of the file?
-//    	String[] out = new String[1];
-//    	
-//    	out[i] = ((SftpFile)files.get(i)).;
-//    	
-//    	if (file.length() != (numSNPs*bytesPerRecord) + headers.length){
-//    		if (file.length() == (numSNPs*bytesPerRecord) + 8){
-//    				
-//    		} else{
-//    			throw new IOException(file + " is not properly formatted.\n(Incorrect length.)");
-//    		}
-//    	}    	
+    public void checkFile(String filename, byte[] headers) throws IOException{
+        //TODO: check the magic numbers
+    	String fileSizeString = dc.getFTP().stat(filename).getSize().toString();
+    	Integer fileSize = Integer.parseInt(fileSizeString);
+    	// check the size of the bnt or bed file using the default format header sizes
+    	if (fileSize != (numSNPs*bytesPerRecord) + headers.length){
+    		// check the size of the bnt or bed file using the Oxford format header sizes
+    		if (fileSize != (numSNPs*bytesPerRecord) + 8){
+    			throw new IOException(filename + " is not properly formatted.\n(Incorrect length.)");
+    		}
+    	}
     }        
 }
